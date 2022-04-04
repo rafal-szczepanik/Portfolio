38590ccf681b3238e54700de1f20982a5a1ea55d
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-04-04 08:07:30 +0200

    update hamburger navigation component

--- src/components/Header/Navigation/HamburgerBtn/HamburgerBtn.js
+++ src/components/Header/Navigation/HamburgerBtn/HamburgerBtn.js
@@ -1,22 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import './HamburgerBtn.css';
 
 export const HamburgerBtn = props => {
-
-
+  const {isOpen, onHandleClick} = props;
   return (
     <button className="HamburgerBtn"
-            onClick={props.onHandleClick}
+            onClick={onHandleClick}
     >
       <span className="HamburgerBtn__bar"
-            style={props.open ? null : {transform: "rotate(-45deg) translate(-15px, 15px) translateY(0)"}}
+            style={isOpen ? {transform: "rotate(-45deg) translate(-10px, 10px) translateY(0)"} : null}
       />
       <span className="HamburgerBtn__bar"
-            style={props.open ? null : {transform: "translateY(-25px)", opacity: "0"}}
+            style={isOpen ? {transform: "translateY(-25px)", opacity: "0"} : null}
       />
       <span className="HamburgerBtn__bar"
-            style={props.open ? null : {transform: "rotate(45deg) translate(0, -15px) translateY(15px)"}}/>
+            style={isOpen ? {transform: "rotate(45deg) translate(-5px, -20px) translateY(15px)"} : null}/>
       />
     </button>
   );
