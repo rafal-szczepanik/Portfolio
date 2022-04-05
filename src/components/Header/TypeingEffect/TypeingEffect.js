244a58a716b8c52fcd9d248b291fe53a8b8a527a
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-04-05 08:22:08 +0200

    update typing component

--- src/components/Header/TypeingEffect/TypeingEffect.js
+++ src/components/Header/TypeingEffect/TypeingEffect.js
@@ -4,14 +4,19 @@ import Typewriter from "typewriter-effect";
 export const TypeingEffect = () => {
   return (
     <h2 className="Header__second-heading"><Typewriter
-      options={{loop: true}}
+      options={{
+        loop: true,
+        deleteSpeed: 40
+      }}
       onInit={(typewriter => typewriter
-          .changeDelay(50)
-          .typeString("NAZYWAM SIĘ <span style='color: #cd53ee'>RAFAŁ</span>")
-          .pauseFor(1000)
-          .deleteAll()
+          .changeDelay(100)
+          .pauseFor(500)
+          .typeString("Nazywam się <span style='color: #cd53ee'>Rafał</span>")
+          .pauseFor(2000)
+          .deleteAll(40)
+          .pauseFor(500)
           .typeString("Szukam stażu jako<span style='color: #cd53ee'> Front-end Developer</span>")
-          .pauseFor(1000)
+          .pauseFor(2000)
           .start()
       )}
     />
