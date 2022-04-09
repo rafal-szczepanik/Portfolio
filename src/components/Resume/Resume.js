e71cab50930ebff34ee54accab34cbc84bfaca54
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-04-09 14:09:36 +0200

    update resume section

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+import {Document, Page, pdfjs} from "react-pdf";
+import {useWebsiteTitle} from "../hooks/useWebsiteTitle";
+import './Resume.css';
+import {RedirectLink} from "../common/RedirectLink/RedirectLink";
+import pdf from './../../assets/images/RafalSzczepanikResume.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+
+export const Resume = () => {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+  useWebsiteTitle("Moje CV");
+
+  return (
+    <div className="Resume__container">
+      <h2>Pobierz moje CV lub <Link className={'Resume__home-link'} to="/">WRÓĆ</Link> na stronę główną</h2>
+
+      <Document className="Resume" file={pdf}>
+        <RedirectLink
+          url={pdf}
+          text={"Download CV"}
+        />
+        <Page pageNumber={1} scale={width > 786 ? 1 : 0.6}/>
+      </Document>
+
+    </div>
+  );
+};
