3fbf56029e36df38fdafac66c8aaa0b1f5988d9f
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-04-11 19:57:13 +0200

    add /.idea to gitignore

---
+++ src/components/About/GitHub/Github.js
@@ -0,0 +1,25 @@
+import React, {useEffect} from "react";
+import GitHubCalendar from "react-github-calendar";
+
+import './Github.css';
+
+export const Github = () => {
+  useEffect(() => {
+    return () => <GitHubCalendar />;
+  }, []);
+  return (
+    <div className="Github__container">
+      <h1 className="Github__heading">
+        Moja <strong className="global__span-color">aktywność</strong>
+      </h1>
+      <GitHubCalendar
+        username="rafal-szczepanik"
+        blockSize={15}
+        blockMargin={5}
+        color="#cd53ee"
+        fontSize={14}
+        style={{color: "white"}}
+      />
+    </div>
+  );
+};
